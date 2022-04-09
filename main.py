b03dce47c4e5c28e0f6680fffe070aa95a5fc1c4
Alexey Rott <alexey.rott.cr@gmail.com>
2022-04-09 17:12:25 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import logging
+import os
+
+import aiogram
+from aiogram import types
+from aiogram.utils.executor import start_webhook
+
+TOKEN = os.getenv('TOKEN')
+bot = aiogram.Bot(TOKEN)
+dispatcher = aiogram.Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+
+# webhook settings
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+
+# webserver settings
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
+
+
+async def on_startup():
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown():
+    await bot.delete_webhook()
+
+
+@dispatcher.message_handler(commands=['echo'])
+async def echo(message: types.Message):
+    await message.answer('Hello world')
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    start_webhook(
+        dispatcher=dispatcher,
+        webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
